767afeb801973ad307e7fcbd3c9e993df4bad2f8
arnav git <shah.arnav@yahoo.com>
2020-04-21 12:15:14 +0530

    keys.js sample file

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+//postgres://username:password@localhost:port/databasename
+
+const connectionString = 'postgres://username:password@localhost:port/databasename';
+modeule.exports = {
+    connectionString
+};
