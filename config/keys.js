aed937cab50609f1715db00dff82d1b05aaa4e79
CaptainArnav <50791015+CaptainArnav@users.noreply.github.com>
2020-04-21 12:20:19 +0530

    empty commit

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-//postgres://username:password@localhost:port/databasename
-
-const connectionString = 'postgres://postgres:qwerty@localhost:5432/test';
-modeule.exports = {
-    connectionString
-};
